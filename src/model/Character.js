0914d872a1760288f628c0638a77873353e40f2c
Weisberg Barros <weisberg.fontes@nlt.com.br>
2020-03-19 09:09:43 -0300

    Created model Character.

---
+++ src/model/Character.js
@@ -0,0 +1,12 @@
+class Character {
+  constructor(id, name, description, thumbnail, isFavorite, isUpdate) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.thumbnail = thumbnail;
+    this.isFavorite = isFavorite;
+    this.isUpdate = isUpdate;
+  }
+}
+
+export default Character;
