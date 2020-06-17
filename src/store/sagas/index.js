67c1f46f591e68481f8e94d886ba4b746fce5264
Weisberg Barros <weisbergbsf@gmail.com>
2020-06-17 17:01:51 -0300

    Refactored code into index file Saga.

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,4 +1,4 @@
-import { takeEvery } from "redux-saga/effects";
+import { takeEvery, all } from "redux-saga/effects";
 
 import * as types from "../types";
 import {
@@ -8,15 +8,17 @@ import {
   fetchSeriesSaga,
   toggleFavoriteSaga,
   fetchStorageCharactersSaga,
-  updateCharacterSaga
+  updateCharacterSaga,
 } from "./characters";
 
 export function* whatchCharacters() {
-  yield takeEvery(types.FETCH_CHARACTERS_BY_NAME, fetchCharactersByNameSaga);
-  yield takeEvery(types.FETCH_CHARACTER_BY_ID, fetchCharacterById);
-  yield takeEvery(types.FETCH_CHARACTERS, fetchCharacters);
-  yield takeEvery(types.FETCH_SERIES, fetchSeriesSaga);
-  yield takeEvery(types.TOGGLE_FAVORITE, toggleFavoriteSaga);
-  yield takeEvery(types.LOAD_FAVORITES, fetchStorageCharactersSaga);
-  yield takeEvery(types.FETCH_CHARACTER_UPDATE, updateCharacterSaga);
+  yield all([
+    takeEvery(types.FETCH_CHARACTERS_BY_NAME, fetchCharactersByNameSaga),
+    takeEvery(types.FETCH_CHARACTER_BY_ID, fetchCharacterById),
+    takeEvery(types.FETCH_CHARACTERS, fetchCharacters),
+    takeEvery(types.FETCH_SERIES, fetchSeriesSaga),
+    takeEvery(types.TOGGLE_FAVORITE, toggleFavoriteSaga),
+    takeEvery(types.LOAD_FAVORITES, fetchStorageCharactersSaga),
+    takeEvery(types.FETCH_CHARACTER_UPDATE, updateCharacterSaga),
+  ]);
 }
